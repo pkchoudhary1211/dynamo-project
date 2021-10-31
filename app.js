a7bcdf32c364054943151700f7a0c2446b779831
prakash choudhary <prakash.choudhary@atriapower.com>
2021-10-31 08:26:07 +0530

    all task done

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 var cors = require('cors')
+
 app.use(cors())
 
 app.use('/', usersRouter);
--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 "use strict";
 const app = require("./app");
 const serverless = require("serverless-http");
-module.exports.hello = serverless(app);
+module.exports.api = serverless(app);
+    
